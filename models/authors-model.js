4def60a58169d74367650b691d57002b60d2b3c7
Alfredo Salazar <>
2020-05-15 06:53:17 -0500

    Preparing class

--- models/authors-model.js
+++ models/authors-model.js
@@ -1,64 +1,7 @@
 const mongoose = require( 'mongoose' );
 
-const authorSchema = mongoose.Schema({
-    firstName : {
-        type : String,
-        required : true
-    },
-    lastName : {
-        type : String,
-        required : true
-    },
-    id : {
-        type: String,
-        unique: true,
-        required : true
-    },
-    password : {
-        type : String,
-        required : true
-    }
-});
 
-const authorCollection = mongoose.model( 'authors', authorSchema );
-
-const Authors = {
-    createAuthor : function( newAuthor ){
-        return authorCollection
-                .create( newAuthor )
-                .then( createdAuthor => {
-                    return createdAuthor;
-                })
-                .catch( err => {
-                    throw new Error( err );
-                });
-    },
-    getAllAuthors : function(){
-        return authorCollection
-                .find()
-                .then( allAuthors => {
-                    return allAuthors;
-                })
-                .catch( err => {
-                    throw new Error( err );
-                });
-    },
-    getAuthorById : function( idUser ){
-        return authorCollection
-                .findOne({ id : idUser })
-                .then( author => {
-                    if( !author ){
-                        throw new Error( "Author not found" );
-                    }
-                    return author;
-                })
-                .catch( err => {
-                    throw new Error( err );
-                });
-
-    }
-}
 
 module.exports = {
-    Authors
+    
 };
--- models/comments-model.js
+++ models/comments-model.js
@@ -1,58 +1,7 @@
 const mongoose = require( 'mongoose' );
 
-const commentSchema = mongoose.Schema({
-    title : {
-        type : String,
-        required : true
-    },
-    content : {
-        type : String,
-        required : true
-    },
-    author : {
-        required : true,
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'authors'
-    }
-});
 
-const commentCollection = mongoose.model( 'comments', commentSchema );
-
-const Comments = {
-    createComment : function( newComment ){
-        return commentCollection
-                .create( newComment )
-                .then( createdComment => {
-                    return createdComment;
-                })
-                .catch( err => {
-                    throw new Error( err );
-                });
-    },
-    getAllComments : function(){
-        return commentCollection
-                .find()
-                .populate( 'author', ['firstName', 'lastName'] )
-                .then( allComments => {
-                    return allComments;
-                })
-                .catch( err => {
-                    throw new Error( err );
-                });
-    },
-    getCommentsByAuthorId : function( authorObjectId ){
-        return commentCollection
-                .find( { author : authorObjectId } )
-                .populate( 'author', ['firstName', 'lastName'])
-                .then( allComments => {
-                    return allComments;
-                })
-                .catch( err => {
-                    throw new Error( err );
-                });
-    }
-}
 
 module.exports = {
-    Comments
+    
 };
