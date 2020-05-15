58666027aade64a6e910e4b043795fbdfecace91
Alfredo Salazar <>
2020-05-15 09:52:18 -0500

    Finished relationships

--- models/authors-model.js
+++ models/authors-model.js
@@ -1,7 +1,56 @@
 const mongoose = require( 'mongoose' );
 
+const authorSchema = mongoose.Schema({
+    firstName : {
+        type : String,
+        required : true
+    },
+    lastName : {
+        type : String,
+        required : true
+    },
+    id : {
+        type : Number,
+        required : true,
+        unique : true
+    }
+});
 
+const authorModel = mongoose.model( 'authors', authorSchema );
+
+const Authors = {
+    createAuthor : function( newAuthor ){
+        return authorModel
+                .create( newAuthor )
+                .then( author => {
+                    return author;
+                })
+                .catch( err => {
+                    throw new Error( err.message );
+                }); 
+    },
+    getAllAuthors : function(){
+        return authorModel
+                .find()
+                .then( authors => {
+                    return authors;
+                })
+                .catch( err => {
+                    throw new Error( err.message );
+                }); 
+    },
+    getAuthorById : function( id ){
+        return authorModel
+                .findOne( { id } )
+                .then( author => {
+                    return author;
+                })
+                .catch( err => {
+                    throw new Error( err.message );
+                }); 
+    }
+}
 
 module.exports = {
-    
+    Authors
 };
--- server.js
+++ server.js
@@ -3,10 +3,140 @@ const mongoose = require( 'mongoose' );
 const morgan = require( 'morgan' );
 const bodyParser = require( 'body-parser' );
 const { DATABASE_URL, PORT } = require( './config' );
-
+const { Authors } = require( './models/authors-model' );
+const { Comments } = require( './models/comments-model' );
 
 const app = express();
 const jsonParser = bodyParser.json();
+
+app.post( '/blog-post/add-author', jsonParser, ( req, res ) => {
+    const { firstName, lastName, id } = req.body;
+
+    // Add validations here
+
+    const newAuthor = {
+        firstName,
+        lastName,
+        id
+    };
+
+    Authors
+        .createAuthor( newAuthor )
+        .then( author => {
+            return res.status( 201 ).json( author );
+        })
+        .catch( err => {
+            res.statusMessage = err.message;
+            return res.status( 400 ).end();
+        });
+});
+
+app.get( '/blog-post/authors', ( req, res ) => {
+    Authors
+        .getAllAuthors()
+        .then( authors => {
+            return res.status( 200 ).json( authors );
+        })
+        .catch( err => {
+            res.statusMessage = err.message;
+            return res.status( 400 ).end();
+        });
+});
+
+app.post( '/blog-post/add-comment', jsonParser, ( req, res ) => {
+    const { title, content, id } = req.body;
+
+    // Validations go here
+
+    Authors
+        .getAuthorById( id )
+        .then( author => {
+
+            if( !author ){
+                // Validation needed here
+            }
+
+            const newComment = {
+                title, 
+                content,
+                author : author._id
+            }
+            
+            Comments
+                .addComment( newComment )
+                .then( comment => {
+                    return res.status( 201 ).json( comment );
+                })
+                .catch( err => {
+                    res.statusMessage = err.message;
+                    return res.status( 400 ).end();
+                });
+        })
+        .catch( err => {
+            res.statusMessage = err.message;
+            return res.status( 400 ).end();
+        });
+});
+
+app.get( '/blog-post/comments', ( req, res ) => {
+    Comments
+        .getAllComments()
+        .then( comments => {
+
+            const filteredComments = comments.map( comment => {
+                return {
+                    title : comment.title,
+                    content : comment.content,
+                    author : `${comment.author.firstName} ${comment.author.lastName}`
+                }
+            });
+            /*
+            const filteredComments = [];
+
+            for( let i = 0; i < comments.length; i ++ ){
+                filteredComments.push({
+                    title : comments[i].title,
+                    content : comments[i].content,
+                    author : `${comments[i].author.firstName} ${comments[i].author.lastName}`
+                })
+            } */
+
+            return res.status( 200 ).json( filteredComments );
+        })
+        .catch( err => {
+            res.statusMessage = err.message;
+            return res.status( 400 ).end();
+        });
+});
+
+app.get( '/blog-post/comments-by-author/:id', ( req, res ) => {
+    const id = req.params.id;
+
+    // Validations required here
+
+    Authors
+        .getAuthorById( id )
+        .then( author => {
+
+            if( !author ){
+                // Send back an error
+            }
+
+            Comments
+                .getCommentsByAuthorId( author._id )
+                .then( comments => {
+                    return res.status( 200 ).json( comments );
+                })
+                .catch( err => {
+                    res.statusMessage = err.message;
+                    return res.status( 400 ).end();
+                });
+        })
+        .catch( err => {
+            res.statusMessage = err.message;
+            return res.status( 400 ).end();
+        });
+});
 
 app.listen( PORT, () =>{
     console.log( "This server is running on port 8080" );
